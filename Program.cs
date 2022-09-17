d65cafa1644746209228cea6844edebda66dd520
Nathaniel <na26435@mail.ru>
2022-09-17 22:29:08 +0300

    Задача №23 Готова

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ System.Console.WriteLine(fifthNum);
 вход координаты двух точек и находит расстояние между ними в 3D */
 
 // Действие 1) Зарандомим координаты точек a и b
-
+/*
 int ax = new Random().Next();
 int ay = new Random().Next();
 int az = new Random().Next();
@@ -52,3 +52,25 @@ double temp = Math.Round(Math.Sqrt(Math.Pow((bx - ax),2) + Math.Pow((by - ay),2)
 // Действие 3) Выведем temp:
 
 System.Console.WriteLine(temp);
+*/
+
+
+/* Задача №23 Напишите программу, которая принимает на вход число (N) 
+и выдает таблицу кубов чисел от 1 до N */
+
+// Действие 1) Зарандомим число (N):
+
+int number = new Random().Next(1,25);
+
+// Действие 2) Выведем это число:
+
+Console.WriteLine(number);
+
+// Действие 3) Создадим таблицу через цикл:
+
+for(int i = 1; i <= number; i++)
+
+// Действие 4) Внутри цикла пропишем Кубическое значение:
+{
+    System.Console.WriteLine(Math.Pow(i,3));
+}
